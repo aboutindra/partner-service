f407d68362ccd986296f10a1af4f516f90166a4f
Andre Febrianto <andrefebrianto88@gmail.com>
2019-11-25 15:17:29 +0700

    add posrgre sql wrapper

---
+++ databases/postgresql/index.js
@@ -0,0 +1,54 @@
+const {Client, Pool} = require('pg');
+const pg = require('pg');
+const config = require('config');
+
+const connectionPool = [];
+const connection = () => {
+  const connectionState = { index: null, config: '', db: null };
+  return connectionState;
+};
+
+
+function addConnectionPool() {
+    if (config.has("PostgreSQL.databases")) {
+        let databases = config.get("PostgreSQL.databases");
+        for (let database of databases) {
+            let newConnection = connection();
+            newConnection.index = database.index;
+            newConnection.config = database.config;
+            connectionPool.push(newConnection);
+        }
+    }
+    
+}
+
+async function createConnectionPool() {
+    for (let connection of connectionPool) {
+        connection.db = new Pool(connection.config);
+    }
+}
+
+function getConnection(index) {
+    for (let connection of connectionPool) {
+        if (connection.index === index) {
+            if (connection.db !== null) {
+                return connection.db;
+            }
+            else {
+                connection.db = new Pool(connection.config);
+                return connection.db;
+            }
+        }
+    }
+    return null;
+}
+
+function init() {
+    addConnectionPool();
+    createConnectionPool();
+}
+
+module.exports = {
+    init,
+    getConnection
+}
