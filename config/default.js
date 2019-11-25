55b7e7b89ad43b9b6654f60b144a738fb8ce9a06
Andre Febrianto <andrefebrianto88@gmail.com>
2019-11-25 15:16:45 +0700

    update default config

--- config/default.js
+++ config/default.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 
 module.exports = {
     PostgreSQL: {
-        dbConfig: {
-            host: process.env.POSTGRE_DB_HOST,
-            port: process.env.POSTGRE_DB_PORT,
-            database: process.env.POSTGRE_DB_DATABASE_NAME,
-            user: process.env.POSTGRE_DB_USERNAME,
-            password: process.env.POSTGRE_DB_PASSWORD
-        }
+        databases: [
+            {
+                index: process.env.POSTGRESQL_DATABASE_PARTNER,
+                config: {
+                    connectionString: process.env.POSTGRESQL_DATABASE_URL
+                }
+            }
+        ]
     }
 } 
