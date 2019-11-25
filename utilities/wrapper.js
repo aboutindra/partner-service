b82fa103989debf54aa4429735e652f2bd03574f
Andre Febrianto <andrefebrianto88@gmail.com>
2019-11-25 15:19:22 +0700

    add wrapper

---
+++ utilities/wrapper.js
@@ -0,0 +1,80 @@
+
+const { NotFoundError, InternalServerError, BadRequestError, ConflictError, ExpectationFailedError,
+  ForbiddenError, GatewayTimeoutError, ServiceUnavailableError, UnauthorizedError } = require('./error');
+const { ERROR:httpError } = require('./httpStatusCode');
+
+const data = (data) => ({ err: null, data});
+
+const paginationData = (data, meta) => ({ err: null, data, meta});
+
+const error = (err) => ({ err, data: [] });
+
+const response = (res, status, result, message = '', code = 200) => {
+  let data = result.data;
+  if(status === false){
+    data = [];
+    message = result.err.message || message;
+    code = checkErrorCode(result.err);
+  }
+  res.status(code).send(
+    {
+      status,
+      data,
+      message,
+      code
+    });
+};
+
+const paginationResponse = (res, type, result, message = '', code = 200) => {
+  let status = true;
+  let data = result.data;
+  if(type === 'fail'){
+    status = false;
+    data = '';
+    message = result.err;
+  }
+  res.send(code,
+    {
+      success: status,
+      data,
+      meta: result.meta,
+      code,
+      message
+    }
+  );
+};
+
+const checkErrorCode = (error) => {
+
+  switch (error.constructor) {
+  case BadRequestError:
+    return httpError.BAD_REQUEST;
+  case ConflictError:
+    return httpError.CONFLICT;
+  case ExpectationFailedError:
+    return httpError.EXPECTATION_FAILED;
+  case ForbiddenError:
+    return httpError.FORBIDDEN;
+  case GatewayTimeoutError:
+    return httpError.GATEWAY_TIMEOUT;
+  case InternalServerError:
+    return httpError.INTERNAL_ERROR;
+  case NotFoundError:
+    return httpError.NOT_FOUND;
+  case ServiceUnavailableError:
+    return httpError.SERVICE_UNAVAILABLE;
+  case UnauthorizedError:
+    return httpError.UNAUTHORIZED;
+  default:
+    return httpError.CONFLICT;
+  }
+
+};
+
+module.exports = {
+  data,
+  paginationData,
+  error,
+  response,
+  paginationResponse
+};
