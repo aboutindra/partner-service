56cdd34ae2013272d9ad3923bd2d59255acdb99c
Andre Febrianto <andrefebrianto88@gmail.com>
2019-11-26 10:45:14 +0700

    add discount handler

--- core/discount.js
+++ core/discount.js
@@ -1,19 +1,94 @@
-const responder = require('../utilities/responder');
+const wrapper = require('../utilities/wrapper');
+const moment = require('moment');
+const { ERROR:erroCode, SUCCESS:successCode } = require('../utilities/httpStatusCode');
+const { NotFoundError,InternalServerError,ConflictError,BadRequestError,ForbiddenError } = require('../utilities/error');
+const Discount = require('../databases/postgresql/models/discount');
+const discount = new Discount(process.env.POSTGRESQL_DATABASE_PARTNER);
 
 const insertDiscount = async (request, response) => {
+    let { code, name, deductionDiscountType, deductionDiscountAmount, additionDiscountType, additionDiscountAmount, startDate, endDate } = request.body;
+    code = code.toUpperCase();
+    if (deductionDiscountType !== 'null') {
+        deductionDiscountAmount = Number(deductionDiscountAmount);
+        if (isNaN(deductionDiscountAmount)) {
+            wrapper.response(response, false, wrapper.error(new BadRequestError("Deduction discount amount must be a number value")));
+            return;
+        }
+    } else {
+        deductionDiscountType = null;
+        deductionDiscountAmount = null;
+    }
 
+    if (additionDiscountType !== 'null') {
+        additionDiscountAmount = Number(additionDiscountAmount);
+        if (isNaN(additionDiscountAmount)) {
+            wrapper.response(response, false, wrapper.error(new BadRequestError("Addition discount amount must be a number value")));
+            return;
+        }
+    } else {
+        additionDiscountType = null
+        additionDiscountAmount = null;
+    }
+
+    startDate = moment(startDate);
+    endDate = moment(endDate);
+
+    if (!startDate.isValid() || !endDate.isValid()) {
+        wrapper.response(response, false, wrapper.error(new BadRequestError("Invalid date value")));
+    } else {
+        startDate = startDate.toDate();
+    }
+
+    let currentProgram = await discount.getActiveDiscount();
+    if (currentProgram.data.length > 0) {
+        wrapper.response(response, false, wrapper.error(new ForbiddenError("There is another program currently running")));
+        return;
+    }
+
+    let result = await discount.insertDiscount(code, name, deductionDiscountType, deductionDiscountAmount, additionDiscountType, additionDiscountAmount, startDate, endDate);
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Discount added", successCode.CREATED);
+    }
 }
 
 const softDeleteDiscount = async (request, response) => {
+    let code = request.params.code.toUpperCase();
 
+    let result = await discount.softDeleteDiscount(code);
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Discount deleted", successCode.OK);
+    }
 }
 
 const getDiscounts = async (request, response) => {
+    let result;
 
+    if (request.query.code) {
+        let code = request.query.code.toUpperCase();
+        result = await discount.getDiscountByCode(code);
+    } else {
+        result = await discount.getAllDiscount();
+    }
+
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Discount(s) retrieved", successCode.OK);
+    }
 }
 
 const getActiveDiscounts = async (request, response) => {
+    let result = await discount.getActiveDiscount();
 
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Active discount retrieved", successCode.OK);
+    }
 }
 
 module.exports = {
---
+++ databases/postgresql/models/discount.js
@@ -0,0 +1,139 @@
+const logger = require('../../../utilities/logger');
+const { NotFoundError,InternalServerError,ConflictError,BadRequestError,ForbiddenError } = require('../../../utilities/error');
+const wrapper = require('../../../utilities/wrapper');
+const postgresqlWrapper = require('../../postgresql');
+
+class Discount {
+    constructor(database) {
+        this.database = database
+    }
+
+    async insertDiscount(code, name, deductionDiscountType = null, deductionDiscountAmount = null, additionDiscountType = null, additionDiscountAmount = null, startDate, endDate) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let insertDiscountQuery = {
+            name: 'insert-discount',
+            text: `INSERT INTO public.discount_program(
+                code, name, deduction_discount_type, deduction_discount_amount, addition_discount_type, addition_discount_amount, is_active, start_date, end_date, created_at, updated_at)
+                VALUES ($1, $2, LOWER($3)::cost_type, $4, LOWER($5)::cost_type, $6, $7, $8, $9, $10, $11);`,
+            values: [code, name, deductionDiscountType, deductionDiscountAmount, additionDiscountType, additionDiscountAmount, true, startDate, endDate, new Date(), new Date()]
+        }
+
+        try {
+            let result = await dbClient.query(insertDiscountQuery);
+            if (result.rowCount === 0) {
+                return wrapper.error(new NotFoundError("Failed add new package"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+            if (error.code === '22P02') {
+                return wrapper.error(new BadRequestError("Invalid type value"));
+            }
+            if (error.code === '23505') {
+                return wrapper.error(new ForbiddenError("Code already exist"));
+            }
+        }
+    }
+
+    async updateDiscount() {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+    }
+
+    async softDeleteDiscount(code) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let deleteDiscountQuery = {
+            name: 'soft-delete-discount-program',
+            text: `UPDATE public.discount_program
+                SET is_active = false, updated_at = $2, deactivated_at = $3
+                WHERE code = $1`,
+            values: [code, new Date(), new Date()]
+        }
+
+        try {
+            let result = await dbClient.query(deleteDiscountQuery);
+            if (result.rowCount === 0) {
+                return wrapper.error(new NotFoundError("Discount not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            console.log(error);
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+
+    async getAllDiscount() {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let getAllDiscountQuery = {
+            name: 'get-discount-history',
+            text: `SELECT * FROM public.discount_program`
+        }
+
+        try {
+            let result = await dbClient.query(getAllDiscountQuery);
+            if (result.rows.length === 0) {
+                return wrapper.error(new NotFoundError("Discount(s) not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+
+    async getDiscountByCode(code) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let getDiscountQuery = {
+            name: 'get-discount',
+            text: `SELECT * FROM public.discount_program
+                WHERE code = $1`,
+            values: [code]
+        }
+
+        try {
+            let result = await dbClient.query(getDiscountQuery);
+            if (result.rows.length === 0) {
+                return wrapper.error(new NotFoundError("Discount not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+
+    async getActiveDiscount() {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let getActiveDiscountQuery = {
+            name: 'get-active-discount-list',
+            text: `SELECT code, name, deduction_discount_type, deduction_discount_amount, addition_discount_type, addition_discount_amount
+                FROM public.discount_program
+                WHERE start_date <= NOW() AND NOW() <= end_date AND is_active = true
+                FETCH FIRST 1 ROWS ONLY`
+        }
+
+        try {
+            let result = await dbClient.query(getActiveDiscountQuery);
+            if (result.rows.length === 0) {
+                return wrapper.error(new NotFoundError("Active discount not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+}
+
+module.exports = Discount;
