b5c0ef5cc6b2c5c063561fae06b985f40c1cc182
Andre Febrianto <andrefebrianto88@gmail.com>
2019-11-26 16:10:29 +0700

    add partner program handler

--- core/partnerProgram.js
+++ core/partnerProgram.js
@@ -1,19 +1,83 @@
-const responder = require('../utilities/responder');
+const wrapper = require('../utilities/wrapper');
+const moment = require('moment');
+const { ERROR:erroCode, SUCCESS:successCode } = require('../utilities/httpStatusCode');
+const { NotFoundError,InternalServerError,ConflictError,BadRequestError,ForbiddenError } = require('../utilities/error');
+const PartnerProgram = require('../databases/postgresql/models/partnerProgram');
+const partnerProgram = new PartnerProgram(process.env.POSTGRESQL_DATABASE_PARTNER);
 
 const insertProgram = async (request, response) => {
+    let { partnerCode, exchangeRate, minAmountPerTransaction, maxAmountPerTransaction, maxTransactionAmountPerDay, maxTransactionAmountPerMonth, startDate, endDate } = request.body;
+    partnerCode = partnerCode.toUpperCase();
 
+    startDate = moment(startDate);
+    endDate = moment(endDate);
+
+    if (!startDate.isValid() || !endDate.isValid()) {
+        wrapper.response(response, false, wrapper.error(new BadRequestError("Invalid date value")));
+    } else {
+        startDate = startDate.toDate();
+    }
+
+    let currentProgram = await partnerProgram.getActivePartnerProgram(partnerCode);
+    if (currentProgram.data.length > 0) {
+        wrapper.response(response, false, wrapper.error(new ForbiddenError("There is another program currently running")));
+        return;
+    }
+
+    let result = await partnerProgram.insertProgram(partnerCode, exchangeRate, minAmountPerTransaction, maxAmountPerTransaction, maxTransactionAmountPerDay, maxTransactionAmountPerMonth, 
+        startDate, endDate);
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Partner program added", successCode.CREATED);
+    }
 }
 
 const softDeleteProgram = async (request, response) => {
-    
+    let id = request.params.id;
+
+    if (isNaN(id)) {
+        wrapper.response(response, false, wrapper.error(new BadRequestError("Id must be an integer value")));
+        return;
+    }
+
+    let result = await partnerProgram.softDeleteProgram(id);
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Partner program deleted", successCode.OK);
+    }
 }
 
 const getPrograms = async (request, response) => {
-    
+    let result;
+
+    if (request.query.id) {
+        let id = parseInt(request.query.id);
+        if (isNaN(id)) {
+            wrapper.response(response, false, wrapper.error(new BadRequestError("Id must be an integer value")));
+            return;
+        }
+        result = await partnerProgram.getProgramById(id);
+    } else {
+        result = await partnerProgram.getAllProgram();
+    }
+
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Partner program(s) retrieved", successCode.OK);
+    }
 }
 
 const getPartnerPrograms = async (request, response) => {
-    
+    let result = await partnerProgram.getPartnerPrograms();
+
+    if (result.err) {
+        wrapper.response(response, false, result);
+    } else {
+        wrapper.response(response, true, result, "Partner program(s) retrieved", successCode.OK);
+    }
 }
 
 module.exports = {
---
+++ databases/postgresql/models/partnerProgram.js
@@ -0,0 +1,167 @@
+const logger = require('../../../utilities/logger');
+const { NotFoundError,InternalServerError,ConflictError,BadRequestError,ForbiddenError } = require('../../../utilities/error');
+const wrapper = require('../../../utilities/wrapper');
+const postgresqlWrapper = require('../../postgresql');
+
+class PartnerProgram {
+    constructor(database) {
+        this.database = database;
+    }
+
+    async insertProgram(partnerCode, exchangeRate, minAmountPerTransaction, maxAmountPerTransaction, maxTransactionAmountPerDay, maxTransactionAmountPerMonth, 
+        startDate, endDate) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let insertPartnerProgramQuery = {
+            name: 'add-new-partner-program',
+        text: `INSERT INTO public.partner_program(
+            partner_code, exchange_rate, start_date, end_date, minimum_amount_per_transaction, maximum_amount_per_transaction, maximum_transaction_amount_per_day,
+            maximum_transaction_amount_per_month, is_active, created_at, updated_at)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`,
+            values: [partnerCode, exchangeRate, startDate, endDate, minAmountPerTransaction, maxAmountPerTransaction, maxTransactionAmountPerDay, maxTransactionAmountPerMonth,
+                true, new Date(), new Date()]
+        }
+
+        try {
+            let result = await dbClient.query(insertPartnerProgramQuery);
+            if (result.rowCount === 0) {
+                return wrapper.error(new NotFoundError("Failed add new partner program"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            console.log(error);
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+            if (error.code === '23505') {
+                return wrapper.error(new ForbiddenError("Code already exist"));
+            }
+            if (error.code === '23503') {
+                return wrapper.error(new ForbiddenError("Partner doesn't exist"));
+            }
+        }
+    }
+
+    async updateProgram() {
+
+    }
+
+    async softDeleteProgram(id) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let deletePartnerProgramQuery = {
+            name: 'soft-delete-partner-program',
+            text: `UPDATE public.partner_program
+                SET is_active = false, updated_at = $2, deactivated_at = $3
+                WHERE id = $1`,
+            values: [id, new Date(), new Date()]
+        }
+
+        try {
+            let result = await dbClient.query(deletePartnerProgramQuery);
+            if (result.rowCount === 0) {
+                return wrapper.error(new NotFoundError("Partner program not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            console.log(error);
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+
+    async getAllProgram() {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let getAllDiscountQuery = {
+            name: 'get-partner-program-list',
+            text: `SELECT * FROM public.partner_program`
+        }
+
+        try {
+            let result = await dbClient.query(getAllDiscountQuery);
+            if (result.rows.length === 0) {
+                return wrapper.error(new NotFoundError("Partner program(s) not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+
+    async getProgramById(id) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let getPartnerProgramQuery = {
+            name: 'get-partner-program',
+            text: `SELECT * FROM public.partner_program
+                WHERE id = $1`,
+            values: [id]
+        }
+
+        try {
+            let result = await dbClient.query(getPartnerProgramQuery);
+            if (result.rows.length === 0) {
+                return wrapper.error(new NotFoundError("Partner program(s) not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+
+    async getPartnerProgram(partnerCode) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let getPartnerProgramQuery = {
+            name: 'get-partner-program-of-partner',
+            text: `SELECT * FROM public.partner_program
+                WHERE partner_code = $1`,
+            values: [partnerCode]
+        }
+
+        try {
+            let result = await dbClient.query(getPartnerProgramQuery);
+            if (result.rows.length === 0) {
+                return wrapper.error(new NotFoundError("Partner program(s) not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+
+    async getActivePartnerProgram(partnerCode) {
+        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let getActiveDiscountQuery = {
+            name: 'get-active-program',
+            text: `SELECT partner_code, exchange_rate, minimum_amount_per_transaction, maximum_amount_per_transaction, maximum_transaction_amount_per_day, maximum_transaction_amount_per_month
+                FROM public.partner_program
+                WHERE start_date <= NOW() AND NOW() <= end_date AND is_active = true AND partner_code = $1
+                FETCH FIRST 1 ROWS ONLY`,
+            values: [partnerCode]
+        }
+
+        try {
+            let result = await dbClient.query(getActiveDiscountQuery);
+            if (result.rows.length === 0) {
+                return wrapper.error(new NotFoundError("Active discount not found"));
+            }
+            return wrapper.data(result.rows);
+        }
+        catch (error) {
+            if (error.code === 'ECONNREFUSED') {
+                return wrapper.error(new InternalServerError("Internal server error"));
+            }
+        }
+    }
+}
+
+module.exports = PartnerProgram;
