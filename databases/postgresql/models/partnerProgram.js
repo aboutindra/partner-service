c811c2a19cf52b6cbee42aba142553d105c74a85
Andre Febrianto <andrefebrianto88@gmail.com>
2019-12-02 14:32:52 +0700

    add transaction process for add partner program

--- databases/postgresql/models/partnerProgram.js
+++ databases/postgresql/models/partnerProgram.js
@@ -10,34 +10,46 @@ class PartnerProgram {
 
     async insertProgram(partnerCode, exchangeRate, minAmountPerTransaction, maxAmountPerTransaction, maxTransactionAmountPerDay, maxTransactionAmountPerMonth, 
         startDate, endDate) {
-        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let dbPool = postgresqlWrapper.getConnection(this.database);
         let insertPartnerProgramQuery = {
             name: 'add-new-partner-program',
-        text: `INSERT INTO public.partner_program(
-            partner_code, exchange_rate, start_date, end_date, minimum_amount_per_transaction, maximum_amount_per_transaction, maximum_transaction_amount_per_day,
-            maximum_transaction_amount_per_month, is_active, created_at, updated_at)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`,
+            text: `INSERT INTO public.partner_program(
+                partner_code, exchange_rate, start_date, end_date, minimum_amount_per_transaction, maximum_amount_per_transaction, maximum_transaction_amount_per_day,
+                maximum_transaction_amount_per_month, is_active, created_at, updated_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`,
             values: [partnerCode, exchangeRate, startDate, endDate, minAmountPerTransaction, maxAmountPerTransaction, maxTransactionAmountPerDay, maxTransactionAmountPerMonth,
                 true, new Date(), new Date()]
         }
 
+        let inserQuotaQuery = {
+            name: 'upsert-quota',
+            text: `INSERT INTO public.partner_quota(
+                partner_code, remaining_deduction_quota_per_day, remaining_deduction_quota_per_month)
+                VALUES ($1, $2, $3)
+                ON CONFLICT (partner_code) DO UPDATE
+                SET remaining_deduction_quota_per_day = $2, remaining_deduction_quota_per_month = $3;`,
+            values: [partnerCode, maxTransactionAmountPerDay, maxTransactionAmountPerMonth]
+        }
+
+        let client = await dbPool.connect();
         try {
-            let result = await dbClient.query(insertPartnerProgramQuery);
+            await client.query('BEGIN');
+            await client.query(insertPartnerProgramQuery);
+            await client.query(inserQuotaQuery);
+            let result = await client.query('COMMIT');
             if (result.rowCount === 0) {
                 return wrapper.error(new NotFoundError("Failed add new partner program"));
             }
             return wrapper.data(result.rows);
         }
         catch (error) {
-            if (error.code === 'ECONNREFUSED') {
-                return wrapper.error(new InternalServerError("Internal server error"));
-            }
             if (error.code === '23505') {
                 return wrapper.error(new ForbiddenError("Code already exist"));
             }
             if (error.code === '23503') {
                 return wrapper.error(new ForbiddenError("Partner doesn't exist"));
             }
+            return wrapper.error(new InternalServerError("Internal server error"));
         }
     }
 
@@ -46,7 +58,7 @@ class PartnerProgram {
     }
 
     async softDeleteProgram(id) {
-        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let dbPool = postgresqlWrapper.getConnection(this.database);
         let deletePartnerProgramQuery = {
             name: 'soft-delete-partner-program',
             text: `UPDATE public.partner_program
@@ -56,42 +68,38 @@ class PartnerProgram {
         }
 
         try {
-            let result = await dbClient.query(deletePartnerProgramQuery);
+            let result = await dbPool.query(deletePartnerProgramQuery);
             if (result.rowCount === 0) {
                 return wrapper.error(new NotFoundError("Partner program not found"));
             }
             return wrapper.data(result.rows);
         }
         catch (error) {
-            if (error.code === 'ECONNREFUSED') {
-                return wrapper.error(new InternalServerError("Internal server error"));
-            }
+            return wrapper.error(new InternalServerError("Internal server error"));
         }
     }
 
     async getAllProgram() {
-        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let dbPool = postgresqlWrapper.getConnection(this.database);
         let getAllDiscountQuery = {
             name: 'get-partner-program-list',
             text: `SELECT * FROM public.partner_program`
         }
 
         try {
-            let result = await dbClient.query(getAllDiscountQuery);
+            let result = await dbPool.query(getAllDiscountQuery);
             if (result.rows.length === 0) {
                 return wrapper.error(new NotFoundError("Partner program(s) not found"));
             }
             return wrapper.data(result.rows);
         }
         catch (error) {
-            if (error.code === 'ECONNREFUSED') {
-                return wrapper.error(new InternalServerError("Internal server error"));
-            }
+            return wrapper.error(new InternalServerError("Internal server error"));
         }
     }
 
     async getProgramById(id) {
-        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let dbPool = postgresqlWrapper.getConnection(this.database);
         let getPartnerProgramQuery = {
             name: 'get-partner-program',
             text: `SELECT * FROM public.partner_program
@@ -100,21 +108,19 @@ class PartnerProgram {
         }
 
         try {
-            let result = await dbClient.query(getPartnerProgramQuery);
+            let result = await dbPool.query(getPartnerProgramQuery);
             if (result.rows.length === 0) {
                 return wrapper.error(new NotFoundError("Partner program(s) not found"));
             }
             return wrapper.data(result.rows);
         }
         catch (error) {
-            if (error.code === 'ECONNREFUSED') {
-                return wrapper.error(new InternalServerError("Internal server error"));
-            }
+            return wrapper.error(new InternalServerError("Internal server error"));
         }
     }
 
     async getPartnerProgram(partnerCode) {
-        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let dbPool = postgresqlWrapper.getConnection(this.database);
         let getPartnerProgramQuery = {
             name: 'get-partner-program-of-partner',
             text: `SELECT * FROM public.partner_program
@@ -123,21 +129,19 @@ class PartnerProgram {
         }
 
         try {
-            let result = await dbClient.query(getPartnerProgramQuery);
+            let result = await dbPool.query(getPartnerProgramQuery);
             if (result.rows.length === 0) {
                 return wrapper.error(new NotFoundError("Partner program(s) not found"));
             }
             return wrapper.data(result.rows);
         }
         catch (error) {
-            if (error.code === 'ECONNREFUSED') {
-                return wrapper.error(new InternalServerError("Internal server error"));
-            }
+            return wrapper.error(new InternalServerError("Internal server error"));
         }
     }
 
     async getActivePartnerProgram(partnerCode) {
-        let dbClient = postgresqlWrapper.getConnection(this.database);
+        let dbPool = postgresqlWrapper.getConnection(this.database);
         let getActiveDiscountQuery = {
             name: 'get-active-program',
             text: `SELECT partner_code, exchange_rate, minimum_amount_per_transaction, maximum_amount_per_transaction, maximum_transaction_amount_per_day, maximum_transaction_amount_per_month
@@ -148,16 +152,14 @@ class PartnerProgram {
         }
 
         try {
-            let result = await dbClient.query(getActiveDiscountQuery);
+            let result = await dbPool.query(getActiveDiscountQuery);
             if (result.rows.length === 0) {
                 return wrapper.error(new NotFoundError("Active discount not found"));
             }
             return wrapper.data(result.rows);
         }
         catch (error) {
-            if (error.code === 'ECONNREFUSED') {
-                return wrapper.error(new InternalServerError("Internal server error"));
-            }
+            return wrapper.error(new InternalServerError("Internal server error"));
         }
     }
 }
