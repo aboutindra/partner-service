38a78f166fa710bc32c169e68f2d5e0f08e3c27a
Andre Febrianto <andrefebrianto88@gmail.com>
2020-01-16 09:45:31 +0700

    remove cron job from application

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const AppServer = require('./routes/routes');
 const logger = require('./utilities/logger');
 const appServer = new AppServer();
 const port = process.env.PORT || 9070;
-const cron = require('node-cron');
+// const cron = require('node-cron');
 const Discount = require('./databases/postgresql/models/discount');
 const discount = new Discount(process.env.POSTGRESQL_DATABASE_PARTNER);
 const PartnerProgram = require('./databases/postgresql/models/partnerProgram');
 const partnerProgram = new PartnerProgram(process.env.POSTGRESQL_DATABASE_PARTNER);
 
-cron.schedule("0 * * * *", async function() {
-    discount.updateDiscountStatus();
-    partnerProgram.updatePartnerProgramStatus();
-});
+// cron.schedule("0 * * * *", async function() {
+//     discount.updateDiscountStatus();
+//     partnerProgram.updatePartnerProgramStatus();
+// });
 
 let listener = appServer.app.listen(port, _ => {
     logger.info("Service started, listening at " + listener.address().port);
