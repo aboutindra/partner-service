dafac341b34b1a07a412b3fb708136d1e3b9600e
Andre Febrianto <andrefebrianto88@gmail.com>
2020-01-17 10:40:38 +0700

    remove unused validator pipeline

--- utilities/validator/issuerPackageValidator.js
+++ utilities/validator/issuerPackageValidator.js
@@ -2,19 +2,15 @@ const { query, param, body } = require('express-validator');
 
 exports.validateInsert = [
     body('name').not().isEmpty().withMessage("Name can not be empty"),
-    body('costType').not().isEmpty().withMessage("Cost type can not be empty")
-    .isIn(['fixed', 'percentage', 'FIXED', 'PERCENTAGE']).withMessage("Cost type can only be fixed or percentage"),
-    body('amount').not().isEmpty().withMessage("Amount can not be empty")
-    .isFloat({ min: 0 }).withMessage("Amount must be positive float")
+    body('costType').not().isEmpty().isIn(['fixed', 'percentage', 'FIXED', 'PERCENTAGE']).withMessage("Cost type can only be fixed or percentage"),
+    body('amount').not().isEmpty().isFloat({ min: 0 }).withMessage("Amount must be positive float")
 ]
 
 exports.validateUpdate = [
     param('id').isInt().withMessage("Id must be positive integer"),
     body('name').not().isEmpty().withMessage("Name can not be empty"),
-    body('costType').not().isEmpty().withMessage("Cost type can not be empty")
-    .isIn(['fixed', 'percentage', 'FIXED', 'PERCENTAGE']).withMessage("Cost type can only be fixed or percentage"),
-    body('amount').not().isEmpty().withMessage("Amount can not be empty")
-    .isFloat({ min: 0 }).withMessage("Amount must be positive integer")
+    body('costType').not().isEmpty().isIn(['fixed', 'percentage', 'FIXED', 'PERCENTAGE']).withMessage("Cost type can only be fixed or percentage"),
+    body('amount').not().isEmpty().isFloat({ min: 0 }).withMessage("Amount must be positive integer")
 ]
 
 exports.validateGet = [
--- utilities/validator/partnerProgramValidator.js
+++ utilities/validator/partnerProgramValidator.js
@@ -2,10 +2,8 @@ const { query, param, body } = require('express-validator');
 const moment = require('moment');
 
 exports.validateInsert = [
-    body('partnerCode').not().isEmpty().withMessage("Partner code can not be empty")
-    .isLength({ max: 5 }).withMessage("Partner code must be maximum 5 characters"),
-    body('exchangeRate').not().isEmpty().withMessage("Exchange rate can not be empty")
-    .isInt({ gt: 0 }).withMessage("Exchange rate must be positive integer greater than 0"),
+    body('partnerCode').not().isEmpty().isLength({ max: 5 }).withMessage("Partner code must be maximum 5 characters"),
+    body('exchangeRate').isInt({ gt: 0 }).withMessage("Exchange rate must be positive integer greater than 0"),
     body('minAmountPerTransaction').optional({ nullable: true }).isInt({ gt: 0 }).withMessage("Minimum amount per transaction must be positive integer greater than 0"),
     body('maxAmountPerTransaction').optional({ nullable: true }).isInt({ gt: 0 }).withMessage("Maximum amount per transaction must be positive integer greater than 0"),
     body('maxTransactionAmountPerDay').optional({ nullable: true }).isInt({ gt: 0 }).withMessage("Maximum transaction amount per day must be positive integer greater than 0"),
@@ -15,10 +13,11 @@ exports.validateInsert = [
 ]
 
 exports.validateDelete = [
-    param('id').isInt().withMessage("Id must be positive integer")
+    param('id').isInt({ min: 1 }).withMessage("Id must be greater than 0")
 ]
 
 exports.validateGet = [
+    query('id').optional({ nullable: true }).isInt({ min: 1 }).withMessage("Id must be filled with integer greater than 0"),
     query('page').optional({ nullable: true }).isInt({ min: 1 }).withMessage("Page must be filled with integer greater than 0"),
     query('limit').optional({ nullable: true }).isInt({ min: 1 }).withMessage("Limit must be filled with integer greater than 0"),
 ]
