e7fa915bb76ef1e06d7186e75b7189749e04da23
Andre Febrianto <andrefebrianto88@gmail.com>
2020-04-07 18:28:47 +0700

    remove cron-job

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ const AppServer = require('./routes/routes');
 const logger = require('./utilities/logger');
 const appServer = new AppServer();
 const port = process.env.PORT || 9070;
-const cron = require('node-cron');
-const Discount = require('./databases/postgresql/models/discount');
-const discount = new Discount(process.env.POSTGRESQL_DATABASE_PARTNER);
-const PartnerProgram = require('./databases/postgresql/models/partnerProgram');
-const partnerProgram = new PartnerProgram(process.env.POSTGRESQL_DATABASE_PARTNER);
-
-cron.schedule("0 * * * *", async function() {
-    console.log("Running cron-job: Updating Discount Status and Partner Program");
-    let response =  await Promise.all([discount.updateDiscountStatus(), partnerProgram.updatePartnerProgramStatus()]);
-    console.log(response);
-});
 
 appServer.app.listen(port, () => {
     logger.info("Service started, listening at " + port);
