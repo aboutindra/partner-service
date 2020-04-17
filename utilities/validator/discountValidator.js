23d8459e581d1fc3dcbca12c4440eb51d052ce5b
Andre Febrianto <andrefebrianto88@gmail.com>
2020-04-17 20:06:05 +0700

    add input validation for discount type percentage

--- utilities/validator/discountValidator.js
+++ utilities/validator/discountValidator.js
@@ -7,6 +7,7 @@ exports.validateInsertDiscount = [
     body('name').isLength({ min: 1, max: 50 }).not().isEmpty().withMessage("Name can not be empty"),
     body('discountType').isIn(CostType.getEnumValues()).withMessage("Discount type must be valid type"),
     body('amount').isInt({ min: 0 }).withMessage("Amount must be greater or equal than zero"),
+    body('amount').if(body('discountType').equals(CostType.PERCENTAGE)).isInt({ min: 0, max: 100 }).withMessage("Discount can only be between 0 and 100"),
     body('startDate').custom(dateValidator.validateDate).withMessage("Start date must be in UTC, ISO-8601, or RFC 2822 format"),
     body('endDate').custom(dateValidator.validateDate).withMessage("End date must be in UTC, ISO-8601, or RFC 2822 format")
 ]
