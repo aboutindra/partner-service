f3b809aa5f96e3a6f41ed545be39bcd81a7ebc08
Andre Febrianto <andrefebrianto88@gmail.com>
2020-06-05 11:19:28 +0700

    add page conversion from string to integer

--- utilities/validator/paginationValidator.js
+++ utilities/validator/paginationValidator.js
@@ -3,10 +3,11 @@ const { BadRequestError } = require('../error');
 
 module.exports = async (req, res, next) => {
 	let limit = null;
-    let skip = null;
+	let skip = null;
+	let page = null;
 
 	if (req.query.page && req.query.limit) {
-		const page = parseInt(req.query.page);
+		page = parseInt(req.query.page);
 		limit = parseInt(req.query.limit);
 		if (isNaN(page) || page < 1 || isNaN(limit) || limit < 1) {
 			let error = wrapper.error(new BadRequestError("Page & Limit must be positive integer value"));
@@ -17,7 +18,7 @@ module.exports = async (req, res, next) => {
 	} else if (req.query.page) {
 		// set limit to default 10 records per page
 		limit = 10;
-		let page = parseInt(req.query.page);
+		page = parseInt(req.query.page);
 		if (isNaN(page) || page < 1) {
 			let error = wrapper.error(new BadRequestError("Page must be positive integer value"));
         	wrapper.response(res, false, error);
@@ -35,5 +36,6 @@ module.exports = async (req, res, next) => {
 
 	req.query.limit = limit;
 	req.query.offset = skip;
+	req.query.page = page;
 	next();
 };
